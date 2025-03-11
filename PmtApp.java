3bfa0497661599ca582abbdca04df57c61673f58
Tadimeti Venkata Sri harshita <harshitatadimeti2002@gmail.com>
2025-03-11 10:25:05 +0530

    Create PmtApp

---
+++ PmtApp.java
@@ -0,0 +1,18 @@
+package pmt.app;
+import pmt.appdata.AppData;
+import pmt.User;
+public class PmtApp {
+
+	public static void main(String[] args) {
+		AppData.initUsers();
+		displayMenu();
+	}
+	public static User getUser(String userName, String password)
+	{
+		User user = new User();
+		user.setUser_name(userName);
+		user.setPassword(password);
+		return user;
+	}
+
+}
