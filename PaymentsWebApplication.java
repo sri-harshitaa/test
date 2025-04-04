92a834cf591fb7bd420c8f62e37ab7c2cf4e77c2
Tadimeti Venkata Sri harshita <harshitatadimeti2002@gmail.com>
2025-04-04 22:26:46 +0530

    Create PaymentsWebApplication

---
+++ PaymentsWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.PaymentsWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentsWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaymentsWebAppApplication.class, args);
+	}
+
+}
