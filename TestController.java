e79638f6bb2dfe217cb22f359328b0b9976cd75f
Tadimeti Venkata Sri harshita <harshitatadimeti2002@gmail.com>
2025-04-04 22:33:42 +0530

    Create TestController

---
+++ TestController.java
@@ -0,0 +1,15 @@
+package com.example.PaymentsWebApp.Controller;
+
+	 import org.springframework.stereotype.Controller;
+	 import org.springframework.web.bind.annotation.RequestMapping;
+
+	 @Controller
+	 public class TestController {
+
+	     @RequestMapping("/test")
+	     public String home(){
+	     	System.out.println("Hello");
+	         return "test";
+	     }
+	 }
+
